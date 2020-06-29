0c84e470006245c14063ee73920a0b09667dec83
Matthew Finlayson <matthewbfinlayson@gmail.com>
2020-06-29 03:18:39 -0600

    separated out scheduler

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='vest',
-    version='0.0.5',
+    version='0.0.6',
     author='mattf1n',
     long_description=long_description,
     long_description_content_type="text/markdown",
