93094f1d960fef30f57eb22964e291acbc29d962
Matthew Finlayson <matthewbfinlayson@gmail.com>
2020-07-01 16:44:44 -0600

    version 0.0.8

--- vest/__init__.py
+++ vest/__init__.py
@@ -3,6 +3,7 @@ import click
 import json
 import os
 import pprint as pp
+from tabulate import tabulate
 import robin_stocks as rs
 
 try:
@@ -19,8 +20,8 @@ class AssetClass():
         self.ticker = setting['Ticker']
         self.equity = 0 if self.ticker not in holdings \
                 else float(holdings[self.ticker]['equity'])
+        self.allocation = 0
         self.target_allocation = float(setting['Target allocation'])
-
         self.target_equity = 0
         self.underfunded = True
 
@@ -34,7 +35,7 @@ class Portfolio():
         self.total_alloc = sum([ac.target_allocation for ac in self.holdings])
         self.normalize_allocations()
         self.cash = max(0,
-                float(rs.build_user_profile()['cash']) 
+                float(rs.load_account_profile()['buying_power']) 
                 - 0.01 - cash_allocation)
         self.find_target_equities(self.total_equity + self.cash, 1)
         self.orders = self.generate_orders()
@@ -46,6 +47,12 @@ class Portfolio():
             if ac.underfunded:
                 ac.target_equity = available_cash * ac.target_allocation \
                         / available_allocation
+        collar = min([ac.target_equity - ac.equity for ac in self.holdings
+            if ac.underfunded]) * 0.07
+        for ac in self.holdings:
+            if ac.underfunded:
+                ac.target_equity -= collar * ac.target_allocation \
+                        / available_allocation 
         complete = True
         for ac in self.holdings:
             if ac.equity > ac.target_equity:
@@ -61,48 +68,61 @@ class Portfolio():
             for ac in self.holdings if ac.target_equity - ac.equity >= 1]
         assert(sum([order['Amount'] for order in orders]) 
                 <= self.total_equity + self.cash)
-        return orders
+        return sorted(orders, key=lambda x: x['Amount'], reverse=True)
+    def show(self):
+        for ac in self.holdings:
+            ac.allocation = round(100 * ac.equity / self.total_equity, 2)
+        print(tabulate({
+            'Category': [ac.name for ac in self.holdings], 
+            'Ticker': [ac.ticker for ac in self.holdings], 
+            'Equity': [ac.equity for ac in self.holdings],
+            'Target allocation': [round(100 * ac.target_allocation) 
+                for ac in self.holdings],
+            'Current allocation': [ac.allocation for ac in self.holdings]},
+            headers='keys'))
+        print(f'Cash: {round(self.cash, 2)}')
 
 def get_config():
     with pkg_resources.open_text(data, 'config.json') as f:
         return json.load(f)
 
-def log_in(config):
+def log_in():
     username = os.environ.get('RH_USERNAME')
     password = os.environ.get('RH_PASS')
     rs.login(username, password)
 
-def rebalance_portfolio(verbose, interactive):
+def rebalance_portfolio():
     ''' Rebalances your portfolio '''
     config = get_config()
-    log_in(config)
+    log_in()
     portfolio = Portfolio(config['Asset classes'], 
             float(config['Cash allocation']))
     open_orders = rs.get_all_open_stock_orders()
     if len(open_orders) > 0:
-        if verbose:
-            click.echo(f'There are already {len(open_orders)} open orders.')
-        if interactive and click.confirm('Show existing orders?'):
+        click.echo(f'There are already {len(open_orders)} open orders.')
+        if click.confirm('Show existing orders?'):
             pp.pprint(open_orders)
-        else:
-            return
-    if verbose:
+    if len(open_orders) == 0 or click.confirm(f'Continue?'):
         pp.pprint(portfolio.orders)
-    if not interactive or click.confirm(f'Execute {len(portfolio.orders)} orders?'):
-        for order in portfolio.orders:
-            if order['Amount'] >= 1:
+        for i, order in enumerate(portfolio.orders):
+            if click.confirm('Execute order for ' 
+                    f'{order["Ticker"]}? ({i+1}/{len(portfolio.orders)})'):
                 result = rs.order_buy_fractional_by_price(
                     order['Ticker'], order['Amount'], timeInForce='gfd')
-                if verbose:
-                    pp.pprint(result)
+                pp.pprint(result)
 
-def cancel_all_orders(verbose, interactive):
+def cancel_all_orders():
     ''' Cancels all orders '''
     config = get_config()
-    log_in(config)
-    if interactive and not click.confirm('Cancel all orders?'):
+    log_in()
+    if not click.confirm('Cancel all orders?'):
         return
     result = rs.cancel_all_stock_orders()
-    if verbose:
-        pp.pprint(result)
+    pp.pprint(result)
 
+def show_allocation():
+    log_in()
+    config = get_config()
+    portfolio = Portfolio(config['Asset classes'],
+            float(config['Cash allocation']))
+    portfolio.show()
--- vest/cli.py
+++ vest/cli.py
@@ -7,18 +7,20 @@ def cli():
     pass
 
 @click.command()
-@click.option('--verbose', is_flag=True)
-@click.option('--interactive', is_flag=True)
-def rebalance(verbose, interactive):
+def rebalance():
     ''' Rebalances your portfolio '''
-    rebalance_portfolio(verbose, interactive)
+    rebalance_portfolio()
 
 @click.command()
-@click.option('--verbose', is_flag=True)
-@click.option('--interactive', is_flag=True)
-def cancel_all(verbose, interactive):
+def cancel_all():
     ''' Cancels all orders '''
-    cancel_all_orders(verbose, interactive)
+    cancel_all_orders()
+
+@click.command()
+def show():
+    ''' Shows current portfolio '''
+    show_allocation()
 
 cli.add_command(rebalance)
 cli.add_command(cancel_all)
+cli.add_command(show)
